4ba82a8ae994d8fa24ee2a4b501ec81e7524afa1
ladogrivi <160308656+ladogrivi@users.noreply.github.com>
2024-04-14 10:49:51 +0400

    add some codes

---
+++ day 05/homework/homework5.py
@@ -0,0 +1,10 @@
+mother_age=34
+father_age=34
+grandma1_age=60
+grandma2_age=54
+my_age=13
+print("in five years my mother will be ",mother_age + 5, "years old")
+print("in five years my father will be",father_age + 5, "years old")
+print("in five years my grandma1 will be", grandma1_age + 5,"years old")
+print("in five years my grandma2 will be", grandma2_age + 5, "years old")
+print("in five yearsi will be", my_age + 5,"years old")
---
+++ day 06/homework/homework.py
@@ -0,0 +1,29 @@
+# name=input("enter your name:")
+# surname=input("enter your surname:")
+# age=input("enter your age:")
+# print("hello my name is",name,"my surname is",surname,"i am ",age,"years old")
+
+
+
+
+# age=int(input("enter your age"))
+# print(age+25)
+
+
+# age=int(input("enter your age:"))
+# number_after_operation=age * 7 / 3
+# print(number_after_operation + 4 * 9)
+
+
+# starting_price=7000
+# increase_percentage=62
+# increased_price=starting_price * (increase_percentage / 100)
+# print("increase in value of the car in  lari:", increased_price)
+
+
+age1=int(input("enter your age:"))
+age2=int(input("enter your age:"))
+age3=int(input("enter your age:"))
+age4=int(input("enter your age:"))
+age5=int(input("enter your age:"))
+print
