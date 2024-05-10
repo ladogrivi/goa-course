a24d85b105bea18ec9bf9005024285586623d585
ladogrivi <160308656+ladogrivi@users.noreply.github.com>
2024-05-10 20:15:41 +0400

    add sum codes

---
+++ day 07/homework/homework.py
@@ -0,0 +1,16 @@
+age1 = int(input("enter first person age:"))
+age2 = int(input("enter second person age:"))
+age3 = int(input("enter third person age:"))
+age4 = int(input("enter fourth person age:"))
+age5 = int(input("enter fifth person age:"))
+
+
+multiply = age1 * age2
+division = multiply / age3
+addition1 = age1 + age2
+addition2 = age4 + age5
+addition3 = addition1 + addition2
+
+print(multiply)
+print(division)
+print(addition3)
---
+++ day 10/hw/homework 10.py
@@ -0,0 +1,44 @@
+# num_1 = 20
+# num_2 = 21
+
+# print(num_1 >= num_2)
+
+# print(num_1 <= num_2)
+ 
+# print(num_1 != num_2)
+
+# print(num_1 == num_2)
+
+# num_1 = 138
+# num_2 = 256
+
+# print(num_1 >= num_2)
+
+# print(num_1 <= num_2)
+
+# print(num_1 != num_2)
+
+# print(num_1 == num_2)
+
+# num_1 = 127 
+# num_2 = 100
+
+# print(num_1 >= num_2) 
+
+# print(num_1 <= num_2)
+
+# print(num_1 != num_2)
+
+# print(num_1 == num_2)
+
+
+for i in range(30):
+    print("goa is the best")
+
+
+
+
+
+
+
+
